a606b3e60fcf62ca80e68becdb8a2247042c3a4e
b1u3too <almadsen792@live.ca>
2022-04-16 04:07:58 +0000

    add DB query using node-postgres pool

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,3 +6,25 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+
+'use strict'
+
+const [cohort, limit] = process.argv.slice(2);
+
+pool.query(`
+  SELECT students.id, students.name, cohorts.name AS cohort_name
+  FROM students
+  JOIN cohorts ON cohorts.id = students.cohort_id  WHERE cohorts.name LIKE '%${cohort}%'
+  LIMIT ${limit || 5}
+`)
+.then(res => {
+  if (res.rows.length === 0) {
+    console.log('No results for that query!');
+  }
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+  });
+})
+.catch(err => {
+  console.error('query error', err.stack)
+});
